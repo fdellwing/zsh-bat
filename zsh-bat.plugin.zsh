a65c2e834a42d3d7bfc363b3ab1846433ee5778a
John P. Lettman <the@johnlettman.com>
2023-08-14 19:08:32 -0400

    add support for Ubuntu and Debian `batcat` naming

--- zsh-bat.plugin.zsh
+++ zsh-bat.plugin.zsh
@@ -1,6 +1,17 @@
 
-if command -v bat >/dev/null 2>&1; then
+if command -v batcat >/dev/null 2>&1; then
+  # Save the original system `cat` under `rcat`
   alias rcat=$(which cat)
+
+  # For Ubuntu and Debian-based `bat` packages
+  # the `bat` program is named `batcat` on these systems
+  alias cat=$(which batcat)
+  export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
+elif command -v bat >/dev/null 2>&1; then
+  # Save the original system `cat` under `rcat`
+  alias rcat=$(which cat)
+
+  # For all other systems
   alias cat=$(which bat)
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 fi
