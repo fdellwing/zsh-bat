467337613c1c220c0d01d69b19d2892935f43e9f
Neil Girdhar <mistersheik@gmail.com>
2024-08-31 01:15:32 -0400

    Fix man headings

--- zsh-bat.plugin.zsh
+++ zsh-bat.plugin.zsh
@@ -7,6 +7,7 @@ if command -v batcat >/dev/null 2>&1; then
   # the `bat` program is named `batcat` on these systems
   alias cat="$(which batcat)"
   export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
+  export MANROFFOPT="-c"
 elif command -v bat >/dev/null 2>&1; then
   # Save the original system `cat` under `rcat`
   alias rcat="$(which cat)"
@@ -14,4 +15,5 @@ elif command -v bat >/dev/null 2>&1; then
   # For all other systems
   alias cat="$(which bat)"
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  export MANROFFOPT="-c"
 fi
